c5cad993b9c25fd631863fd10fcee788ff09a94e
oogwayoncoke <ameen.sayed.nac@gmail.com>
2026-02-16 18:55:29 +0200

    Delete backend/backend directory

--- backend/backend/backend/authentication/adapter.py
+++
@@ -1,5 +0,0 @@
-from allauth.account.adapter import DefaultAccountAdapter
-class AccountAdapter(DefaultAccountAdapter):
-  def get_email_confirmation_url(self, request, emailconfirmation):
-    return f'http://localhost:5173/verify-email/{emailconfirmation.key}'
-  
