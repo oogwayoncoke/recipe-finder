c24f21d626df32b17711bed204905070cbf71825
oogwayoncoke <ameen.sayed.nac@gmail.com>
2026-02-17 20:28:01 +0200

    fix models

--- backend/shops/models/base.py
+++ backend/shops/models/base.py
@@ -1,5 +1,4 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
 import uuid 
 
 class Tenant(models.Model):
@@ -18,28 +17,9 @@ class Tenant(models.Model):
 class TenantModel(models.Model):
     tenant = models.ForeignKey(
         Tenant, 
-        on_commit=models.CASCADE, 
-        db_index=True 
+        db_index=True, 
+        on_delete=models.CASCADE
     )
 
     class Meta:
         abstract = True
-class Customer(TenantModel):
-  tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, 
-                             related_name='customers')
-  full_name = models.CharField(max_length=255, null=False)
-  phone_number = PhoneNumberField(region='EG',null=False)
- 
-  def __str__(self):
-    return self.full_name
-  
-class Technician(TenantModel):
-    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE,
-                               null=False)
-    full_name = models.CharField(max_length=255, null=False)
-    role = models.CharField(max_length=50, null=False)
-    hourly_rate = models.DecimalField(max_digits=10, decimal_places=2,
-                                      default=0.00,
-                                    null=False)
-    def __str__(self):
-      return f'{self.full_name}({self.role} - ${self.hourly_rate}/hr)'
