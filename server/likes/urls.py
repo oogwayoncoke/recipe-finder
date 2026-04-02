7b0ceb2e423ace47e10cec3e612205bd99aa643d
abduallahnagy-afk <abduallahnagy88@gmail.com>
2026-04-02 17:50:51 +0200

    fixed problems likes app

--- server/likes/urls.py
+++ server/likes/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from .views import FavoriteToggleView, FavoriteListView, FavoriteStatusView
+from likes.views import FavouriteListView, FavouriteToggleView, FavouriteStatusView
 
 urlpatterns = [
     # GET  /favorites/                            → full favorites list
-    path('', FavoriteListView.as_view(), name='favorite-list'),
+    path('', FavouriteListView.as_view(), name='favorite-list'),
 
     # POST   /favorites/recipes/<external_id>/    → save recipe
     # DELETE /favorites/recipes/<external_id>/    → unsave recipe
-    path('recipes/<int:external_id>/', FavoriteToggleView.as_view(), name='favorite-toggle'),
+    path('recipes/<int:external_id>/', FavouriteToggleView.as_view(), name='favorite-toggle'),
 
     # GET  /favorites/recipes/<external_id>/status/  → { is_favorited: bool }
-    path('recipes/<int:external_id>/status/', FavoriteStatusView.as_view(), name='favorite-status'),
+    path('recipes/<int:external_id>/status/', FavouriteStatusView.as_view(), name='favorite-status'),
 ]
